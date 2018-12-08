a2ff2a5d17cf13df73bbcee5f8ee34443e341b1e
adonese <mmbusif@student.uofk.edu>
2018-12-08 16:19:39 +0200

    adding webhooks for noebs

---
+++ hooks.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+	"gopkg.in/go-playground/webhooks.v5/github"
+)
+
+const (
+	path = "/webhooks"
+)
+
+func main() {
+	hook, _ := github.New(github.Options.Secret("953507cbb10c25e9284040d4def099f0c57d1813"))
+
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// ok event wasn;t one of the ones asked to be parsed
+			}
+		}
+		switch payload.(type) {
+
+		case github.ReleasePayload:
+			release := payload.(github.ReleasePayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", release)
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+	http.ListenAndServe(":3000", nil)
+}
